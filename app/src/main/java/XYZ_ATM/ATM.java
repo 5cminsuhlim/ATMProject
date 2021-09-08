81e8078c7c191471bba1cd930b5de164ef3fe301
Eric <5cminsuhlim@gmail.com>
2021-09-08 14:11:29 +1000

    Update ATM.java
    -Set up constructor and method headers

---
+++ app/src/main/java/XYZ_ATM/ATM.java
@@ -0,0 +1,62 @@
+package XYZ_ATM
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LocalDate;
+
+public class ATM{
+
+    private HashMap<double, int> balance;
+    private ArrayList<Card> validCards;
+    private LocalDate date;
+
+    public ATM(HashMap<double, int> balance, ArrayList<Card> validCards, LocalDate date) {
+        this.balance = balance;
+        this.validCards = validCards;
+        this.date = LocalDate.now();
+    }
+
+    private boolean isValid(Card c){
+
+    }
+
+    private boolean checkCardNumber(Card c){
+        return c.length() == 5 && validCards.contains(c);
+    }
+
+    private boolean checkPin(Card c, String userPin){
+        return userPin == c.getPin();
+    }
+
+    private boolean checkExpDate(Card c){
+
+    }
+
+    private boolean checkIssDate(Card c){
+
+    }
+
+    private String apologize(){
+
+    }
+
+    private String error(){
+
+    }
+
+    private void withdraw(){
+
+    }
+
+    public void deposit(){
+
+    }
+
+    public double checkBalance(){
+
+    }
+
+    public boolean promptUser(){ //checking to proceed / cancel
+
+    }
+}
