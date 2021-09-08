fb16c3e1291bc95be45e2069e7bb73375c3b35b3
Sean Kunkler <skun3933@uni.sydney.edu.au>
2021-09-08 14:12:24 +1000

    Add CardNumberGenerator - Basic

---
+++ cardNumberGen.py
@@ -0,0 +1,14 @@
+import random
+import sys
+if(len(sys.argv) > 1 and isinstance((int(sys.argv[1])), int)):
+    i = 0
+    text_file = open("output.txt", "w")
+    while(i<int(sys.argv[1])):
+        current_random = [str(random.randint(0,9)),str(random.randint(0,9)),str(random.randint(0,9)),str(random.randint(0,9)),str(random.randint(0,9))]
+        joined_string = "".join(current_random)
+        joined_string = joined_string + "\n"
+        text_file.write(joined_string)
+        i += 1
+    text_file.close()
+else:
+    print("Enter Correct Args - Num of Cards")
