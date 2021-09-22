4cdfc17196a22d131aa7e90b541e564dda6db97f
Sean Kunkler <skun3933@uni.sydney.edu.au>
2021-09-22 15:40:41 +1000

    Update - bug fix

--- app/src/test/java/XYZ_ATM/AppTest.java
+++ app/src/test/java/XYZ_ATM/AppTest.java
@@ -556,7 +556,7 @@ class AppTest {
         ATM testATM2 = new ATM(balance2, testCards, testUserList,testDate);
         assertEquals(-1,testATM.withdraw(testUser, userOverBalance),"Insufficent User Funds");
         assertEquals(-2,testATM.withdraw(testUser2, atmOverBalance),"Insufficent ATM Funds");
-        assertEquals(0,testATM2.withdraw(testUser2, smallAmmount),"Insufficent ATM Funds (Bad Denominations");
+        assertEquals(-2,testATM2.withdraw(testUser2, smallAmmount),"Insufficent ATM Funds (Bad Denominations");
         assertEquals(-3,testATM.withdraw(testUser, userNonValidAmmount),"Invalid Ammount Entered");
         assertEquals(0,testATM.withdraw(testUser, validAmmount),"Withdrawl Error");
     }
